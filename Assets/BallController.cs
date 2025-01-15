97694495ce03337200545af979a9169461194253
Juan Ablan <ablanjuan1@gmail.com>
2025-01-15 11:15:02 -0800

    UnityEvent Implementation

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 2f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,21 +14,26 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector += Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector += Vector2.right;
-        }
+        //Handled by the Input Manager now
+        // Vector2 inputVector = Vector2.zero;
+        // if (Input.GetKey(KeyCode.W)) {
+        //     inputVector += Vector2.up;
+        // }
+        // if (Input.GetKey(KeyCode.S)) {
+        //     inputVector += Vector2.down;
+        // }
+        // if (Input.GetKey(KeyCode.A)) {
+        //     inputVector += Vector2.left;
+        // }
+        // if (Input.GetKey(KeyCode.D)) {
+        //     inputVector += Vector2.right;
+        // }
 
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        
+    }
+
+    public void moveBall(Vector2 input) {
+        Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
