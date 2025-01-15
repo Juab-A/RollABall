35a59c9a72999ca12d12bf53553b4ec2df18a468
Juan Ablan <ablanjuan1@gmail.com>
2025-01-15 11:36:09 -0800

    Jumping Mechanic

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,9 +1,15 @@
+using UnityEditor;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float jumpHeight = 2f;
+
+    private float raycastDistance = 0.6f;
+    [SerializeField] private LayerMask groundLayer;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,7 +35,17 @@ public class BallController : MonoBehaviour
         //     inputVector += Vector2.right;
         // }
 
+        if (Input.GetKey(KeyCode.Space) && IsGrounded()) {
+            Debug.Log("Should Jump");
+            sphereRigidbody.AddForce(Vector3.up * jumpHeight);
+        }
         
+    }
+
+    public  bool IsGrounded()
+    {
+        RaycastHit hit;
+        return Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer);
     }
 
     public void moveBall(Vector2 input) {
